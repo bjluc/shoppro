4478ef86c7af50cfabb274b7b3f8d90b26d40215
Luc Audemard <bjluc@bjluc.co.uk>
2020-10-22 20:38:30 +0100

    backend - register user

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -32,6 +32,15 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await compare(enteredPassword, this.password)
 }
 
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next()
+  }
+
+  const salt = await pkg.genSalt(10)
+  this.password = await pkg.hash(this.password, salt)
+})
+
 const User = mongoose.model('User', userSchema)
 
 export default User
