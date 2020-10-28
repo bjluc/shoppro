9ed72d340fc55d1542ae8ff4ebd0ede6df98a7f0
Luc Audemard <bjluc@bjluc.co.uk>
2020-10-28 20:20:49 +0000

    order screen and order action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -12,7 +12,10 @@ import {
   userDetailsReducer,
   userUpdateProfileReducer,
 } from './reducers/userReducers'
-import { orderCreateReducer } from './reducers/ordeReducers'
+import {
+  orderCreateReducer,
+  orderDetailsReducer,
+} from './reducers/ordeReducers'
 
 const reducer = combineReducers({
   productList: productListReducer,
@@ -23,6 +26,7 @@ const reducer = combineReducers({
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
   orderCreate: orderCreateReducer,
+  orderDetails: orderDetailsReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
