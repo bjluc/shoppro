6e534e60eecdc78a289781aae0718efa2db70a0d
Luc Audemard <bjluc@bjluc.co.uk>
2020-11-04 22:39:10 +0000

    admin middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -102,4 +102,13 @@ const updateUserProfile = asyncHandler(async (req, res) => {
     throw new Error('User not found')
   }
 })
-export { authUsers, getUserProfile, registerUser, updateUserProfile }
+
+//  @desc   Get all users
+//  @route  GET /api/users
+//  @access Private/Admin
+const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({})
+  res.json(users)
+})
+
+export { authUsers, getUserProfile, registerUser, updateUserProfile, getUsers }
